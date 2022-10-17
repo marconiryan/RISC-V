b1593df3568d54445686c099e971e79a75cc4f17
Ryan <86175804+marconiryan@users.noreply.github.com>
2022-10-17 00:44:25 -0300

    for

--- macros.asm
+++ macros.asm
@@ -2,10 +2,15 @@
 	li a7, 1
 	addi a0, %x, 0
 	ecall
-	.end_macro
+.end_macro
 
 .macro read_int(%rg)
 	li a7,5
 	ecall
 	addi %rg, a0, 0
+.end_macro
+
+.macro for(%contador, %condicao_parada, %loop,%desvio)
+	BGE %contador,%condicao_parada, %desvio
+	j %loop
 .end_macro
